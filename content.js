85e239e863dd0d22c661d4b15039a0becdd917b7
Jennifer Brisbane <jmbrisbane01@gmail.com>
2017-10-15 13:56:13 -0400

    fixed issue with lazyload images not changing on wapo

--- content.js
+++ content.js
@@ -2,12 +2,13 @@
 // These names will eventually come from a database
 var names = [   ["Donald J. Trump","Dorothy Zbornak"],
                 ["Donald Trump","Dorothy Zbornak"],
-                ["Trump","Dorothy Zbornak"],
                 ["Mr. Trump","Ms. Zbornak"],
+                ["Trump","Dorothy Zbornak"],
                 ["Melania Trump","Frieda Claxton"],
                 ["Melania","Frieda Claxton"],
                 ["Michael Pence","Sophia Petrillo"],
                 ["Mike Pence","Sophia Petrillo"],
+                ["Mr. Pence","Mrs. Petrillo"],
                 ["Pence","Petrillo"],
                 ["Betsy Devos","Rose Nylund"],
                 ["Devos","Nylund"],
@@ -70,76 +71,83 @@ var names = [   ["Donald J. Trump","Dorothy Zbornak"],
                 ["The White House","Shady Pines"],
                 ["White House","Shady Pines"],
                 ["Pennsylvania Avenue","Biscayne Boulevard"],
-             
-
             ];
 
 
 chrome.runtime.sendMessage({checkForEnabled: "Performing a check"}, function(response) {
     var enabled = response.enabled;
-    toggleNames(enabled);
+    toggle(enabled);
 });
     
 
 
-function toggleNames(enabled) {
-
-    if (enabled) {
-        replacePhotos();
-    } else {
-        removePhotos();
-    }
+function toggle(enabled) {
 
     var elements = document.getElementsByTagName('*');
 
-    var beforeName;
-    var afterName;
-    
-    for (var i = 0; i < names.length; i++) {
-
-        if (enabled) {
-            beforeName = names[i][0];
-            afterName = names[i][1];
-        } else {
-            beforeName = names[i][1];
-            afterName = names[i][0];
-        }
-        
-        
         for (var j = 0; j < elements.length; j++) {
             var element = elements[j];
-
 
             for (var k = 0; k < element.childNodes.length; k++) {
                 var node = element.childNodes[k];
 
                 if (node.nodeType === 3) {
-                    replaceNames(node, element, beforeName, afterName, enabled);
+                    // replaceNames(node, element, beforeName, afterName, enabled);
+                    replaceNames(node, enabled);
                 }
             }
         }
+    
+    if (enabled) {
+        replacePhotos();
+    } else {
+        //removePhotos();
     }
 }
 
 
-var galleryUrl = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=714d94e01a2f8290b2e404dc4e4da6a3&gallery_id=152943881-72157686071664832&extras=url_m&format=json&nojsoncallback=1";
 
-var theGirlsPhotos = [];
 
 function replacePhotos() {
+
+    const galleryUrl = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=714d94e01a2f8290b2e404dc4e4da6a3&gallery_id=152943881-72157686071664832&extras=url_m&format=json&nojsoncallback=1";
+
+    let theGirlsPhotos = [];
+    
     $.getJSON(galleryUrl, function(data) {
         $.each(data.photos.photo, function(i,item){
             theGirlsPhotos.push(item.url_m);
         });
     
-        console.log("theGirlsPhotos.length" + theGirlsPhotos.length);
+        console.log("theGirlsPhotos.length: " + theGirlsPhotos.length);
         
         var images = document.getElementsByTagName('img');
         var photoIndex = 0;
 
-        for (var i = 0, l = images.length; i < l; i++) {    
-            photoIndex = Math.floor(Math.random() * 29) + 0; // currently 30 total in the gallery 
-            images[i].src = theGirlsPhotos[photoIndex];
+        for (var i = 0, l = images.length; i < l; i++) {
+            
+            //if ($(images[i]).attr('class').indexOf('logo')) {
+            
+            //if (!$(images[i]).hasClass("main-logo")) {
+            if (!$(images[i]).hasClass("main-logo") && !$(images[i]).hasClass("wplogo")) {
+                
+                    // remove lazyloader attributes otherwise these pics get loaded after DOM load
+					if (images[i].hasAttribute('data-src')){
+						images[i].removeAttribute('data-src');	
+					};
+					if (images[i].hasAttribute('data-hi-res-src')){
+						images[i].removeAttribute('data-hi-res-src');	
+					};
+					if (images[i].hasAttribute('data-low-res-src')){
+						images[i].removeAttribute('data-low-res-src');	
+					};
+                
+                
+                    photoIndex = Math.floor(Math.random() * 50) + 0; // 50 total in the gallery 
+                    images[i].src = theGirlsPhotos[photoIndex];
+            }
+            
+
         }
     });
 }
@@ -154,15 +162,27 @@ function removePhotos() {
     }
 }
 
-function replaceNames(node, element, before, after, enabled) {
+// function replaceNames(node, element, before, after, enabled) {
+function replaceNames(node, enabled) {
 
+    var beforeName;
+    var afterName;
+
+    for (var i = 0; i < names.length; i++) {        
+
+        if (enabled) {
+            beforeName = names[i][0];
+            afterName = names[i][1];
+        } else {
+            beforeName = names[i][1];
+            afterName = names[i][0];
+        }
+        
         //alert("in replaceNames, node.nodeValue: " + node.nodeValue);
         var text = node.nodeValue;
     
-    
-        var regExInput = new RegExp(before, "gi");
-        var replacedText = text.replace(regExInput, after); 
-    
+        var regExInput = new RegExp(beforeName, "gi");
+        var replacedText = text.replace(regExInput, afterName); 
         
         if (enabled) {
             if (text == text.toUpperCase()) {
@@ -171,11 +191,12 @@ function replaceNames(node, element, before, after, enabled) {
         }
     
         if (replacedText !== text) {
-            element.replaceChild(document.createTextNode(replacedText), node);
+            //element.replaceChild(document.createTextNode(replacedText), node);
+            node.nodeValue = replacedText;
         } else {
             //alert("in the else, we changed nothing!");
-        }
-        
+        }       
+    }
 }
 
 
