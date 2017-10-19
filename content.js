15c368ed13e3271f516be722cbf8ddab9904f0f4
Jennifer Brisbane <jmbrisbane01@gmail.com>
2017-10-18 22:27:32 -0400

    modified to use firebase names, and changed browser action to page action

--- content.js
+++ content.js
@@ -1,123 +1,124 @@
 
-// These names will eventually come from a database
-var names = [   ["POTUS","Dorothy Zbornak"],
-                ["Donald J. Trump","Dorothy Zbornak"],
-                ["Donald Trump","Dorothy Zbornak"],
-                ["Mr. Trump","Ms. Zbornak"],
-                ["Trump","Dorothy Zbornak"],
-                ["Melania Trump","Cousin Magda Yitchinson"],
-                ["Melania","Cousin Magda"],
-                ["Mrs. Trump","Cousin Magda"],
-                ["FLOTUS","Cousin Magda"],
-                ["Michael Pence","Stanley Zbornak"],
-                ["Mike Pence","Stan Zbornak"],
-                ["Mr. Pence","Mr. Zbornak"],
-                ["Pence","Stanley Zbornak"],
-                ["Betsy Devos","Rose Nylund"],
-                ["Devos","Nylund"],
-                ["Ben Carson","Miles Webber"],
-                ["Carson","Webber"],
-                ["Jeff Sessions","Big Daddy Hollingsworth"],
-                ["Sessions","Big Daddy Hollingsworth"],
-                ["Nikki Haley","Frieda Claxton"],
-                ["Haley","Claxton"],
-                ["Steve Bannon","Mario Lopez (Dorothy's prized pupil)"],
-                ["Bannon","Mario Lopez"],
-                ["Jared Kushner","Little Michael Zbornak"],
-                ["Kushner","Michael Zbornak"],
-                ["Ivanka Trump","Barbara Thorndyke"],
-                ["Ivanka","Barbara"],
-                ["Scaramucci","Peterson"],
-                ["Anthony Scaramucci","Arnie Peterson"],
-                ["John Kelly","Truby Steele"],
-                ["Mr. Kelly","Ms. Steele"],
-                ["Sarah Huckabee Sanders","Bessie the Showbiz Chicken"],
-                ["Sarah Sanders","Bessie the Showbiz Chicken"],
-                ["Sanders","Bessie"],
-                ["Mike Huckabee","Dreyfuss the Dog"],
-                ["Huckabee","Dreyfuss"],
-                ["Kellyanne Conway","Daisy the Sunshine Cadet"],
-                ["Conway","Daisy"],
-                ["Vladimir V. Putin", "Coco the Gay Cook"],
-                ["Vladimir Putin", "Coco the Gay Cook"],
-                ["Putin","Coco the Gay Cook"],
-                ["Kim Jong-un","Johnny No-Thumbs"],
-                ["Kim Jong un","Johnny No-Thumbs"],
-                ["Jong-un","No-Thumbs"],
-                ["Rex Tillerson","Rex Huntington"],
-                ["Tillerson","Huntington"],
-                ["Jong-un","No-Thumbs"],
-                ["Stephen Miller","Raoul (Sophia's date to the banquet)"],
-                ["Miller","Raoul"],
-                ["Robert Mueller","Detective Sergeant Al Mullens"],
-                ["Robert S. Mueller","Detective Sergeant Al Mullens"],
-                ["Special Counsel Mueller","Detective Sergeant Mullens"],
-                ["Special Counsel Robert Mueller","Detective Sergeant Mullens"],
-                ["Special Counsel Robert S. Mueller","Detective Sergeant Mullens"],
-                ["Mr. Mueller","Detective Sergeant Mullens"],
-                ["Mueller","Mullens"],
-                ["James Comey","Bobby Hopkins"],
-                ["Comey","Hopkins"],
-                ["Neil Gorsuch","Little Sven from Sweden"],
-                ["Mr. Gorsuch","Little Sven"],
-                ["Gorsuch","Sven"],
-                ["Paul Ryan","Bobby Hopkins (played by a young George Clooney)"],
-                ["Ryan","Bobby Hopkins (a.k.a. George Clooney)"],
-                ["Mitch McConnell","Ham Lushbaugh"],
-                ["McConnell ","Lushbaugh"],
-                ["Andrew McCabe ","Enrique Mas"],
-                ["McCabe","Mas"],
-                ["James Mattis","Enrique Mas"],
-                ["Mattis","Mas"],
-                ["Eric Hargan", "Sophia's friend Lillian (who couldn't afford a safe nursing home)"],
-                ["Mr. Hargan", "Ms. Lillian"],
-                ["Washington, D.C.","St. Olaf"],
-                ["The Capitol","St. Olaf"],
-                ["Capitol Hill","St. Olaf"],
-                ["Pentagon","Rusty Anchor"],
-                ["The White House","Shady Pines"],
-                ["White House","Shady Pines"],
-                ["Pennsylvania Avenue","Biscayne Boulevard"],
-            ];
+toggleNames(enabled);
+toggleImages(enabled);
 
+function toggleNames(enabled) {
+    
+    const namesUrl = "https://golden-times.firebaseio.com/.json";
 
-chrome.runtime.sendMessage({checkForEnabled: "Performing a check"}, function(response) {
-    var enabled = response.enabled;
-    toggle(enabled);
-});
+    let namePairs = [];
+    let elements = document.getElementsByTagName('*');
+    
+    var storyHeadings = $(".story-heading");
     
 
+    
 
-function toggle(enabled) {
+    
+    $.getJSON(namesUrl, function(data) {
+        $.each(data, function(i,item){
+            let namePair = [item.before, item.after];
+            namePairs.push(namePair);
+        });
+    
+        // console.log("namePairs.length: " + namePairs.length);
 
-    var elements = document.getElementsByTagName('*');
+        for (let i=0; i < namePairs.length; i++) {
+            
+            // console.log("origName: " + namePairs[i][0] + " \nafterName: " + namePairs[i][1]);
+            
+            if (enabled) {
+                beforeName = namePairs[i][0];
+                afterName = namePairs[i][1];
+            } else {
+                beforeName = namePairs[i][1];
+                afterName = namePairs[i][0];
+            }
+            
 
-        for (var j = 0; j < elements.length; j++) {
-            var element = elements[j];
+            
+            for (let i = 0; i < storyHeadings.length; i++) {
+        
+                var storyHeading = storyHeadings[i];
+        
+                for (var j = 0; j < storyHeading.childNodes.length; j++) {
+                    var node = storyHeading.childNodes[j];
 
-            for (var k = 0; k < element.childNodes.length; k++) {
-                var node = element.childNodes[k];
+                    if (node.nodeType === 3 && node.nodeValue) {
+                        
+                        var text = node.nodeValue;
+                        var regExInput = new RegExp(beforeName, "gi");
+                        node.nodeValue = text.replace(regExInput, afterName); 
+                    }
+                }
+            }
+            
+            
+            
+            
+            for (var j = 0; j < elements.length; j++) {
+                
+                var element = elements[j];
 
-                if (node.nodeType === 3) {
-                    // replaceNames(node, element, beforeName, afterName, enabled);
-                    replaceNames(node, enabled);
+                for (var k = 0; k < element.childNodes.length; k++) {
+                    var node = element.childNodes[k];
+
+                    if (node.nodeType === 3 && node.nodeValue) {
+
+                        var text = node.nodeValue;
+                        var regExInput = new RegExp(beforeName, "gi");
+                        node.nodeValue = text.replace(regExInput, afterName); 
+        
+//                        if (enabled) {
+//                            if (text == text.toUpperCase()) {
+//                                replacedText.toUpperCase();
+//                            } 
+//                        }    
+                    }
                 }
             }
         }
+        
+
+
+    });
+}
+
+
+
+// function replaceNames(node, element, before, after, enabled) {
+function replaceNames(node, origName, newName, enabled) {
+
+    for (var i = 0; i < names.length; i++) {        
+
+        if (enabled) {
+            beforeName = names[i][0];
+            afterName = names[i][1];
+        } else {
+            beforeName = names[i][1];
+            afterName = names[i][0];
+        }
+
+        var text = node.nodeValue;
     
-    if (enabled) {
-        replacePhotos();
-    } else {
-        //removePhotos();
+        var regExInput = new RegExp(beforeName, "gi");
+        var replacedText = text.replace(regExInput, afterName); 
+        
+        if (enabled) {
+            if (text == text.toUpperCase()) {
+                replacedText.toUpperCase();
+            } 
+        }
+    
+        node.nodeValue = replacedText;
     }
 }
 
 
 
-
-function replacePhotos() {
-        
-    var images = document.querySelectorAll("img", "figure");        
+function toggleImages(enabled) {
+    
+    var images = document.querySelectorAll("img", "figure", "gwd-taparea", "iframe");        
         
     for (var i = 0, l = images.length; i < l; i++) {
             
@@ -134,59 +135,36 @@ function replacePhotos() {
 				images[i].removeAttribute('data-low-res-src');	
             }
                 
-            var index = Math.floor(Math.random() * 121) + 0; // currently 122 photos available
-            //images[i].src = theGirlsPhotos[photoIndex];
-            images[i].src = chrome.extension.getURL('/images/golden' + index + '.jpg');
+            if (images[i].hasAttribute('data-threshold')){
+				images[i].removeAttribute('data-threshold');	
+            }
+            
+            // remove srcset attributes
+            if (images[i].hasAttribute('srcset')) {
+                images[i].removeAttribute('srcset');	
+            }
+
+
+            if (!images[i].hasAttribute('orig-src')) {
+                $(images[i]).attr('orig-src',images[i].src);                
+            }
+
+            if (!images[i].hasAttribute('new-src')) {
+                var index = Math.floor(Math.random() * 121) + 0; // currently 122 photos available
+                $(images[i]).attr('new-src', chrome.extension.getURL('/images/golden' + index + '.jpg'));
+            }
+
+            if (enabled) {                     
+                images[i].src = $(images[i]).attr("new-src");
+            } else {
+                images[i].src = $(images[i]).attr("orig-src");
+            }
+            
         }
     }
 }
 
-function removePhotos() {
-    
-    
-    var images = document.getElementsByTagName('img');
-    
-    for (var i = 0, l = images.length; i < l; i++) {    
-        images[i].src = "";
-    }
-}
 
-// function replaceNames(node, element, before, after, enabled) {
-function replaceNames(node, enabled) {
-
-    var beforeName;
-    var afterName;
-
-    for (var i = 0; i < names.length; i++) {        
-
-        if (enabled) {
-            beforeName = names[i][0];
-            afterName = names[i][1];
-        } else {
-            beforeName = names[i][1];
-            afterName = names[i][0];
-        }
-        
-        //alert("in replaceNames, node.nodeValue: " + node.nodeValue);
-        var text = node.nodeValue;
-    
-        var regExInput = new RegExp(beforeName, "gi");
-        var replacedText = text.replace(regExInput, afterName); 
-        
-        if (enabled) {
-            if (text == text.toUpperCase()) {
-                replacedText.toUpperCase();
-            } 
-        }
-    
-        if (replacedText !== text) {
-            //element.replaceChild(document.createTextNode(replacedText), node);
-            node.nodeValue = replacedText;
-        } else {
-            //alert("in the else, we changed nothing!");
-        }       
-    }
-}
 
 
 
